043d3257c993e3a1021c58134a98bb94b043915f
mitutee <volman1285@gmail.com>
2017-05-12 17:23:26 +0300

    Base functionality added: exec terminal commands; run js scripts;

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('Express');
+const bodyParser = require('body-parser');
 
 let app = express();
 const port = 4444;
 
-const apiController = require('./controllers/apiController');
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
 
+const apiController = require('./controllers/apiController');
 apiController(app); // Fire up the controller
 
 app.get('/', function(req, res){
@@ -13,3 +19,4 @@ app.get('/', function(req, res){
 
 app.listen(port);
 console.log('App is listening on the port ' + port);
+
