8710cae5c91d35734c5aee7f77c07de3886d70ce
Mikhail <mitutee@gmail.com>
2018-05-09 12:12:10 +0300

    Implement simple host with scotty

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for simplehost
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import           Data.Semigroup                ((<>))
+import           Network.Wai.Middleware.Static
+import           Options.Applicative
+import           Web.Scotty                    (middleware, scotty)
+
+data Config = Config
+    { directory :: String
+    , port      :: Int }
+
+config :: Parser Config
+config = Config
+    <$> strOption
+        (  long "directory"
+        <> short 'd'
+        <> help "Directory to serve" )
+    <*> option auto
+        ( long "port"
+        <> short 'p'
+        <> help "Port to listen to"
+        <> showDefault
+        <> value 3000
+        <> metavar "INT")
+
+main :: IO ()
+main = run =<< execParser opts
+  where
+    opts = info (config <**> helper)
+      ( fullDesc
+     <> progDesc "Serve your directory with static files"
+     <> header "simpehost" )
+
+run :: Config -> IO ()
+run (Config d p) = do
+    scotty p $ do
+        middleware $ staticPolicy (noDots >-> addBase d)
+
